b84dd827bffa11abd6e90e3bc5d03a398857866f
jjz <woaf1003@gmail.com>
2014-10-08 10:22:16 +0800

    rename

---
+++ libjpeg-turbo-android/use-libjpeg-turbo-android/obj/local/armeabi/objs/bitherjni/bitherlibjni.o.d
@@ -0,0 +1,155 @@
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/obj/local/armeabi/objs/bitherjni/bitherlibjni.o: \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/bitherlibjni.c \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/string.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/bitmap.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/log.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/setjmp.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/setjmp.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/math.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/limits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/limits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/limits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/limits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/page.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/time.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/time.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/time.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/siginfo.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jpeglib.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jconfig.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jmorecfg.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/cdjpeg.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jinclude.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/memory.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jpeglib.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jerror.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/cderror.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jversion.h \
+ /Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/android/config.h
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/bitmap.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/log.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/setjmp.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/setjmp.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/math.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/limits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/limits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/limits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/limits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/page.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/time.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/time.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/time.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jpeglib.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jconfig.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jmorecfg.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/cdjpeg.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jinclude.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jpeglib.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jerror.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/cderror.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/jversion.h:
+
+/Users/nn/Documents/workspace/bither-android-lib/libjpeg-turbo-android/use-libjpeg-trubo-adnroid/jni/libjpeg-turbo/android/config.h:
---
+++ libjpeg-turbo-android/use-libjpeg-turbo-android/obj/local/armeabi/objs/piutil/crypt.o.d
@@ -0,0 +1,86 @@
+/Users/nn/Documents/workspace/testLibjpeg/obj/local/armeabi/objs/piutil/crypt.o: \
+ /Users/nn/Documents/workspace/testLibjpeg/jni/crypt.c \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdio.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdint.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/stddef.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/compiler.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/types.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/kernel.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdlib.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/string.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/malloc.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/alloca.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/strings.h \
+ /Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/memory.h \
+ /Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/aes.h \
+ /Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/opensslconf.h \
+ /Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/rand.h \
+ /Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/ossl_typ.h \
+ /Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/e_os2.h
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdio.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdint.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/stddef.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/compiler.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/asm/types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/linux/types.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/machine/kernel.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/stdlib.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/string.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/malloc.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/alloca.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/strings.h:
+
+/Users/nn/Documents/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/memory.h:
+
+/Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/aes.h:
+
+/Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/opensslconf.h:
+
+/Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/rand.h:
+
+/Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/ossl_typ.h:
+
+/Users/nn/Documents/workspace/testLibjpeg/jni/crypto/openssl/e_os2.h:
